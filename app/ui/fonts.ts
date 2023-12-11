4b8945dcf5fdafc9f1aa0fee3916b83bdc736b72
Kuldeep Rawat <kuldeep.rawat@true-fan.in>
2023-12-11 14:40:44 +0530

    first commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,5 @@
+import { Inter, Lusitana } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'] });
+
+export const lusitana = Inter({ subsets: ['latin'], weight: ['400', '700'] });
