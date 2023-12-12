fb232dd5e9ea64c10501b6f076ed32e4138ed984
Kuldeep Rawat <kuldeep.rawat@true-fan.in>
2023-12-12 18:22:32 +0530

    implemented own debounce function

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,8 +1,32 @@
 'use client';
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
+import { usePathname, useRouter, useSearchParams } from 'next/navigation';
 
 export default function Search({ placeholder }: { placeholder: string }) {
+  const searchParams = useSearchParams();
+  const {replace} = useRouter();
+  const pathname = usePathname();
+
+  const debounceFunc = (func:(...args:any[])=>void,delay:number)=>{
+    let timer:any;
+    return (...args:[])=>{
+      clearTimeout(timer);
+      timer = setTimeout(()=>{
+        func.apply(this,args);
+      },delay)
+    }
+  }
+
+  const handleSearch = debounceFunc((query:string)=>{
+    const params = new URLSearchParams(searchParams)
+    if(query){
+      params.set('query',query)
+    }else{
+      params.delete('query')
+    }
+    replace(`${pathname}?${params.toString()}`)
+  },300)
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
@@ -11,6 +35,8 @@ export default function Search({ placeholder }: { placeholder: string }) {
       <input
         className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
         placeholder={placeholder}
+        defaultValue={searchParams.get("query")?.toString()}
+        onChange={(e)=>handleSearch(e.target.value)}
       />
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
     </div>
