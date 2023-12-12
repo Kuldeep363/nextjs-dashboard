48f78924169a7ca00d75baa0092504255f048682
Kuldeep Rawat <kuldeep.rawat@true-fan.in>
2023-12-12 18:59:36 +0530

    debounce parameter error resolved

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -5,28 +5,29 @@ import { usePathname, useRouter, useSearchParams } from 'next/navigation';
 
 export default function Search({ placeholder }: { placeholder: string }) {
   const searchParams = useSearchParams();
-  const {replace} = useRouter();
+  const { replace } = useRouter();
   const pathname = usePathname();
 
-  const debounceFunc = (func:(...args:any[])=>void,delay:number)=>{
-    let timer:any;
-    return (...args:[])=>{
+  const debounceFunc = (func: (...args:any[])=>void, delay: number):Function => {
+    let timer: NodeJS.Timeout;
+    return function (this: Function, ...args: []) {
       clearTimeout(timer);
-      timer = setTimeout(()=>{
-        func.apply(this,args);
-      },delay)
-    }
-  }
+      timer = setTimeout(() => {
+        func.apply(this, args);
+      }, delay);
+    };
+  };
 
-  const handleSearch = debounceFunc((query:string)=>{
-    const params = new URLSearchParams(searchParams)
-    if(query){
-      params.set('query',query)
-    }else{
-      params.delete('query')
+  const handleSearch = (query: string): void => {
+    const params = new URLSearchParams(searchParams);
+    if (query) {
+      params.set('query', query);
+    } else {
+      params.delete('query');
     }
-    replace(`${pathname}?${params.toString()}`)
-  },300)
+    replace(`${pathname}?${params.toString()}`);
+  };
+  const debouncedSearch = debounceFunc(handleSearch, 300);
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
@@ -35,8 +36,8 @@ export default function Search({ placeholder }: { placeholder: string }) {
       <input
         className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
         placeholder={placeholder}
-        defaultValue={searchParams.get("query")?.toString()}
-        onChange={(e)=>handleSearch(e.target.value)}
+        defaultValue={searchParams.get('query')?.toString()}
+        onChange={(e) => debouncedSearch(e.target.value)}
       />
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
     </div>
